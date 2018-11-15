907096d2e9b48eb5cf48c6d15b49462949819e9c
muath <gharablipro19933@hotmail.com>
2018-11-14 23:39:39 -0800

    add

--- Verification.sol
+++ Verification.sol
@@ -4,41 +4,39 @@ contract Verification
 
         string [] hash;
         
+        
+        
+        
         struct owner
         {
             
-            string username;
-            string password;
+            bytes32 username;
+            bytes32 password;
         }
         struct generalInfo
         {
-        string firstName;
-        string lastName;
-        string age;
-        string sex;
-        string gpa;
-        string major;
-        string universityName;
-        string nationalID;
-        string dateOfBirth;
-        string placeOfBirth;
-        string uploader;
+        string tx;
+        bytes32 firstName;
+        bytes32 lastName;
+       bytes32 [9] moreInfo;
+       
+       
         }
-        mapping (string => owner) ownerMapping;
+        mapping (bytes32 => owner) ownerMapping;
         mapping (string =>generalInfo) mappingInfo;
         mapping (string =>bool) mappingCheckSignedupBefore;
         
         
         string url;
-        mapping (string=>string)mappingTrans;
+        mapping (string=>generalInfo)mappingTrans;
         
-        function addOwner (string _username,string _password) public
+        function addOwner (bytes32 _username,bytes32 _password) public
         {
             ownerMapping[_username]=owner(_username,_password);
 
         }
        
-        function checkSignedBefore(string _username)public view returns(string,string){
+        function checkSignedBefore(bytes32 _username)public view returns(bytes32,bytes32){
             return (ownerMapping[_username].username,ownerMapping[_username].password);
         }
         
@@ -46,60 +44,6 @@ contract Verification
         
         
 
-       function getDateOfBirth(string _hash) public view returns (string)
-       {
-           return mappingInfo[_hash].dateOfBirth;
-       }
-  
-       function getPlaceOfBirth(string _hash) public view returns (string)
-       {
-           return mappingInfo[_hash].placeOfBirth;
-       }
-        
- 
-       function getNationalID(string _hash) public view returns (string)
-       {
-           return mappingInfo[_hash].nationalID;
-       }
-
-       function getFirstName(string _hash) public view returns (string)
-       {
-           return mappingInfo[_hash].firstName;
-       }
-       
-
-       function getLastName(string _hash) public view returns (string)
-       {
-           return mappingInfo[_hash].lastName;
-       }
-
-       function getAge(string _hash) public view returns (string)
-       {
-           return mappingInfo[_hash].age;
-       }
-
-       function getSex(string _hash) public view returns (string)
-       {
-           return mappingInfo[_hash].sex;
-       }
-       
-
-       function getMajor(string _hash) public view returns (string)
-       {
-           return mappingInfo[_hash].major;
-       }
-
-       function getGPA(string _hash) public view returns (string)
-       {
-           return mappingInfo[_hash].gpa;
-       }
-       
-   
-       function getUniversityName(string _hash) public view returns (string)
-       {
-           return mappingInfo[_hash].universityName;
-       }
-       
        
        
         function checkHashExsist(string _hast) public  view  returns  (bool)
@@ -119,18 +63,28 @@ contract Verification
             url=_url;
         }
        
-        function addHash(string _hash,string _firstName,string _lastName,string _age,string _sex,string _gpa,string _major,string _universityName ,string _nationalID,string _dateOfBirth,string _placeOfBirth,string uploader) public
+        function addHash(string tx,string _hash,bytes32 _firstName,bytes32 _lastName,bytes32 _age,bytes32 _sex,bytes32 _gpa,bytes32 _major,bytes32 _universityName ,bytes32 _nationalID,bytes32 _dateOfBirth,bytes32 _placeOfBirth,bytes32 uploader) public
         
         {
             hash.push(_hash);
-            mappingInfo[_hash]=generalInfo(_firstName,_lastName,_age,_sex,_gpa,_major,_universityName,_nationalID,_dateOfBirth,_placeOfBirth,uploader);
+            bytes32  [9] memory moreInfo  =[_age,_sex,_gpa,_major,_universityName,_nationalID,_dateOfBirth,_placeOfBirth,uploader];
+            mappingInfo[_hash]=generalInfo(tx,_firstName,_lastName , moreInfo );
+            
+            
+            
+            
+            
+            
+            
+            
+            
+  
          
            
+          
+           
         }
-        function saveTransaction(string _hash,string _transaction) public 
-        {
-              mappingTrans[_hash]=_transaction;
-        }
+       
         function getUrl() public view returns (string)
         {
             return url;
@@ -149,10 +103,38 @@ contract Verification
         {
             return hash[index];
         }
-        function getTransactions(string _hash)public view returns(string)
-        {
-            return mappingTrans[_hash];
+        
+        
+        function getAllInformation(string _hash) public view returns(string ,
+        bytes32,bytes32,bytes32[9]){
+            
+            return (
+                mappingTrans[_hash].tx,
+                mappingTrans[_hash].firstName,
+                mappingTrans[_hash].lastName,
+                mappingTrans[_hash].moreInfo
+               );
+              
+                
+           
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+               
+            
+            
         }
+      
+        
+        
+    
 
        
 }
